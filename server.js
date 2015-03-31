9079f48ceb4c2ea8230bd1d65a31c13968822e3e
Christopher Van <cvan@mozilla.com>
2015-03-31 11:31:47 -0700

    add routing for `/room/:room?`

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
 
+var utils = require('./lib/utils');
+
 // Socket.io mapping.
 var sockets = {};
 
@@ -9,6 +11,12 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
+
+// Routes that should render `/index.html` and get routed on the client.
+const SPA_ROUTES = [
+  '/room/:room'
+];
+
 
 app.use('/js/', express.static(__dirname + '/build/js'));
 app.use('/css/', express.static(__dirname + '/build/css'));
@@ -24,6 +32,23 @@ io.on('connection', function (socket) {
 
   socket.on('move', function (data) {
   });
+});
+
+app.get('/room/:room?', function (req, res, next) {
+  var roomName = req.params.room;
+
+  if (!roomName) {
+    res.redirect('/room/' + utils.randomString());
+    return;
+  }
+
+  next();
+});
+
+SPA_ROUTES.forEach(function (route) {
+  app.get(route, function (req, res) {
+    res.sendFile(__dirname + '/src/index.html');
+  })
 });
 
 server.listen(port, function () {
